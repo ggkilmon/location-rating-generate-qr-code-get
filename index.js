aab1597a02d90a0e55d2717eff6db3dafee83964
Graham Kilmon <ggkilmon@gmail.com>
2022-01-06 22:24:24 -0500

    generate qr code and save to s3

--- index.js
+++ index.js
@@ -1,4 +1,52 @@
+const { AwesomeQR } = require('awesome-qr');
+const { v4:uuidv4  } = require('uuid');
+var AWS = require('aws-sdk');
+
+var ssm = new AWS.SSM();
+var s3 = new AWS.S3();
+
 exports.handler = async function(event, context) {
-    console.log("Event: \n" + JSON.stringify(event, null, 2));
-    return context;
+    //get cloudfront url
+    var paramBaseUrl = await getParam('/location-rating/base-url');
+    var baseUrl = paramBaseUrl.Parameter.Value;
+
+    //get bucket
+    var paramBucket = await getParam('/location-rating/qr-bucket');
+    var bucket = paramBucket.Parameter.Value;
+
+    //generate guid
+    var guid = uuidv4();
+
+    //generate qr code
+    const buffer = await new AwesomeQR({
+        text: `${baseUrl}?qr-guid=${guid}`,
+        size: 500,
+    }).draw();
+
+    //save to s3
+    await saveQr(buffer, bucket, guid);
+
+    return {
+        body: `{"qr-code-url": "https://${bucket}.s3.amazonaws.com/${guid}.png"}`,
+        bodyEncoding: 'text',
+        headers: {
+            'Content-Type': 'application/json',
+        },
+        status: 200,
+    };
+}
+
+async function getParam(name) {
+    return await ssm.getParameter({
+        Name: name,
+    }).promise();
+}
+
+async function saveQr(buffer, bucket, guid){
+    await s3.putObject({
+        Body: buffer,
+        Bucket: bucket,
+        Key: `${guid}.png`,
+        ContentType: 'image/png',
+    }).promise();
 }
